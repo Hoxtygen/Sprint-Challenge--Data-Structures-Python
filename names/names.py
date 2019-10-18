7004d01c168b4e8893e3c63991e9d995efc20af1
Hoxtygen <hoxtygen@live.com>
2019-10-18 09:34:35 +0100

    Speed up name checking time complexity

--- names/names.py
+++ names/names.py
@@ -10,11 +10,11 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+dict_for_name_1 = dict((name, True) for name in names_1)
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+for name_2 in names_2:
+    if name_2 in  dict_for_name_1:
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
